14defb2db55d244067e6c3cc541aad9740361fb4
RizkyZulkarnaen <rizkizul38@gmail.com>
2024-11-14 16:24:10 +0700

    refactor navigation and add search component; update theme to Lara

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,14 +3,14 @@ import App from './App.vue';
 import router from './router';
 import './index.css';
 import PrimeVue from 'primevue/config';
-import Aura from '@primevue/themes/aura';
+import Lara from '@primevue/themes/lara';
 import 'primeicons/primeicons.css';
 
 const app = createApp(App);
 app.use(router);
 app.use(PrimeVue,{
     theme: {
-        preset: Aura
+        preset: Lara
     }
 });
 app.mount('#app');

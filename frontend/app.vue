0734711a7702c114270bacce209860dcb88d6dde
RizkyZulkarnaen <rizkizul38@gmail.com>
2024-11-16 18:29:11 +0700

    initialize Nuxt.js project with Tailwind CSS setup and remove unused files

---
+++ frontend/app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <!-- <NuxtRouteAnnouncer />
+    <NuxtWelcome /> -->
+    <h1>
+      Welcome to the Nuxt.js + Vuetify.js + TypeScript + Jest + E2E Cypress
+      Starter
+    </h1>
+  </div>
+</template>
